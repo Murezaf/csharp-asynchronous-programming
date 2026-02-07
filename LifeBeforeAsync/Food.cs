0fddb290a301bccd8c0c0507f12eac11bcfc3c5a
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2026-02-08 01:27:38 +0330

    BeforeAsync

---
+++ LifeBeforeAsync/Food.cs
@@ -0,0 +1,32 @@
+﻿namespace LifeBeforeAsync;
+
+public abstract class Food
+{
+    readonly TimeSpan _cookTime;
+    public string Name { get; }
+
+    protected Food(TimeSpan cookTime)
+    {
+        _cookTime = cookTime;
+        Name = GetType().Name;
+    }
+
+    public async Task Cook()
+    {
+        Console.WriteLine($"Cooking {Name}...");
+        await Task.Delay(_cookTime);
+        //Task.Delay(_cookTime).Wait();
+        Console.WriteLine($"{Name} Complete");
+    }
+}
+
+//public class Turkey : Food
+//{
+//    public Turkey() : base(TimeSpan.FromMicroseconds(5))
+//    {
+//    }
+//}
+public class Turkey() : Food(TimeSpan.FromSeconds(5));
+public class MashedPotatoes() : Food(TimeSpan.FromSeconds(2));
+public class Gravy() : Food(TimeSpan.FromSeconds(1));
+public class Stuffing() : Food(TimeSpan.FromSeconds(2));
---
+++ LifeBeforeAsync/Program.cs
@@ -0,0 +1,15 @@
+﻿using LifeBeforeAsync;
+
+Console.WriteLine("Hello, World!");
+
+var turkey = new Turkey();
+//turkey.Cook();
+//Console.WriteLine("Main finished!");
+
+turkey.Cook().ContinueWith(_ =>
+{
+    var gravy = new Gravy();
+    gravy.Cook();
+});
+
+Console.ReadLine();
