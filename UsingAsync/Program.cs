3d5a9e5e16f4256e435b3256353820a3230b26bb
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2026-02-08 02:31:26 +0330

    UsingAsync and ParallelProgramming

---
+++ UsingAsync/Program.cs
@@ -0,0 +1,11 @@
+﻿using UsingAsync;
+
+Console.WriteLine("Hello World!");
+
+var turkey = new Turkey();
+await turkey.Cook();
+
+var gravy = new Gravy();
+await gravy.Cook();
+
+Console.WriteLine("Ready to eat");
