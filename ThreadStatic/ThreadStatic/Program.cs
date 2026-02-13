0e95c70ac62bd66331a1093020e08da03d6cd53d
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2026-02-13 19:40:03 +0330

    Thread Static

---
+++ ThreadStatic/ThreadStatic/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadStatic
+{
+
+    internal class Program
+    {
+        [ThreadStatic]
+        static int threadSpecificValue;
+
+        static void Main(string[] args)
+        {
+            threadSpecificValue = 100;
+
+            Console.WriteLine($"Main thread - threadSpecificValue: {threadSpecificValue}");
+
+            Thread thread1 = new Thread(ThreadMethod);
+            Thread thread2 = new Thread(ThreadMethod);
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine($"Main thread after threads finished - threadSpecificValue: {threadSpecificValue}");
+        }
+
+        static void ThreadMethod()
+        {
+            threadSpecificValue = new Random().Next(1, 100);
+
+            Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} {nameof(threadSpecificValue)}: {threadSpecificValue}");
+        }
+    }
+}
